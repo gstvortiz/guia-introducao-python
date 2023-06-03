b2fd49796535b4058ba75d1e2d5041b29ecbf105
gstvortiz <gstvortiz@hotmail.com>
2023-06-03 19:06:14 -0300

    #004 - Listas

--- Desafio 4/01_VogaisTuplas.py
+++ Desafio 4/01_VogaisTuplas.py
@@ -2,3 +2,10 @@
 #   Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
 frutas = ('banana', 'abacate', 'pera', 'uva', 'abacaxi')
+
+for palavra in frutas:
+    lista = []
+    for letra in palavra:
+        if letra in 'AaEeIiOoUu':
+            lista.append(letra)
+    print(palavra, ':', lista)
--- Desafio 4/02_TuplasTimes.py
+++ Desafio 4/02_TuplasTimes.py
@@ -1,11 +1,16 @@
 #   Com as tuplas dos 20 primeiros colocados da Liga Juvenil de Futebol Amador,
 #   ordenados de acordo com sua colocação, escreva na tela:
-#       a) Os 5 primeiros times.
-#       b) Os últimos 4 colocados.
-#       c) Times em ordem alfabética.
-#       d) Em que posição está o time da Chapecoense.
+#       print('Os 5 primeiros times.
+#       print('Os últimos 4 colocados.
+#       print('Times em ordem alfabética.
+#       print('Em que posição está o time da Chapecoense.
 
 times = ('Palmeiras', 'Santos', 'Flamengo', 'Atlético', 'Internacional',
          'Atlético-PR', 'Botafogo', 'Goias', 'Corinthians', 'Grêmio',
          'Bahia', 'São Paulo', 'Ceará SC', 'Fortaleza', 'Vasco da Gama',
          'Cruzeiro', 'Fluminense', 'Chapecoence', 'CSA', 'Avaí')
+
+print('Os 5 primeiros times são:', times[:5])
+print('Os últimos 4 colocados são:', times[-4:])
+print('Times em ordem alfabética:', sorted(times))
+print('Em que posição está o time da Chapecoense:', times.index('Chapecoence') + 1)
