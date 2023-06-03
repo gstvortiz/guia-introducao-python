864baa66ba42342ab9313bbce0f93a0edaf45ab5
gstvortiz <gstvortiz@hotmail.com>
2023-06-03 20:14:58 -0300

    #006 - Strings

--- Desafio 6/01_Modulos.py
+++ Desafio 6/01_Modulos.py
@@ -3,20 +3,19 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Printar o Sistema Operacional que voce esta usando:
-# YOUR CODE HERE
+print(sys.platform)
 
 # Printar a versão de Python que voce esta usando:
-# YOUR CODE HERE
+print(sys.version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
 # Printar a process Id atual
-# YOUR CODE HERE
+print(os.getpid())
 
 # Printar o atual diretório:
-# YOUR CODE HERE
+print(os.getcwd())
 
 # Printar o nome da maquina
-# YOUR CODE HERE
+print(os.uname().nodename)
--- Desafio 6/02_Numpy.py
+++ Desafio 6/02_Numpy.py
@@ -1,36 +1,32 @@
-
 import numpy as np
 
 #1- Crie uma matriz 1D com números de 0 a 9
-
+matriz = np.arange(9)
 
 #2- Crie uma matriz booleana numpy 3×3 com ‘True’
-
+np.full(fill_value = True, shape = (3, 3))
 
 #3- Extraia todos os números ímpares de ‘arr’
-
 arr = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+impares = arr[(arr % 2).astype(bool)]
 
 #4- Substitua todos os números ímpares arr por -1
-
+arr = np.where(arr % 2 != 0, arr, -1)
 
 #5- Substitua todos os números ímpares em arr com -1 sem alterar arr
-
 arr = np.arange(10)
-#out = np.where()
+print(np.where(arr % 2 != 0, arr, -1))
 
 #6- Converta uma matriz 1D para uma matriz 2D com 2 linhas:
-
 arr = np.arange(10)
+arr = arr.reshape(2, 5)
 
-       
-#6- Empilhe matrizes verticalmente:
-
+#7- Empilhe matrizes verticalmente:
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
+c = np.concatenate([a, b])
 
-
-#6- Empilhe as matrizes horizontalmente:
-
+#8- Empilhe as matrizes horizontalmente:
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
+c = np.concatenate([a, b], axis = 1)
