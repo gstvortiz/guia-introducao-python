2866c9f2d95736979e3e80c4420f7d7449abe0e3
gstvortiz <gstvortiz@hotmail.com>
2023-06-03 19:10:55 -0300

    #005 - Tuplas

--- Desafio 5/01_OcorrenciaString.py
+++ Desafio 5/01_OcorrenciaString.py
@@ -2,3 +2,9 @@
 #       - Quantas vezes aparece a letra 'A'
 #       - Em que posição ela aparece a primeira vez
 #       - Em que posição ela aparece a última vez
+
+frase = input('Digite sua frase: ')
+
+print(f'A letra "A" aparece {frase.upper().count("A")} vezes na frase "{frase}"')
+print(f'A letra "A" aparece pela primeira vez na posição {frase.upper().find("A")} na frase "{frase}"')
+print(f'A letra "A" aparece pela última vez na posição {frase.upper().rfind("A")} na frase "{frase}"')
