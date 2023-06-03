a99d97ebc5460064c70c9d6ae5c66acf87dbae8c
gstvortiz <gstvortiz@hotmail.com>
2023-06-03 20:36:56 -0300

    #008 - Arquivos

--- Desafio 7/01_File.py
+++ Desafio 7/01_File.py
@@ -4,9 +4,18 @@ referencia: https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writ
 
 # 1-Abra o arquivo do diretorio atual "foo.txt"
 # Printe todo o conteudo do arquivo , então feche o arquivo
+arq = open('foo.txt', 'r')
+for linha in arq:
+    print(linha)
+arq.close()
 
 # 2- Crie um arquivo chamado "bar.txt" 
 # Abra o arquivo e conte quanta vezes a palavra "sir" aparece 
 # Escreva no arquivo que voce criou quantas palavras foram encontradas
 # Feche o arquivo
-
+contador = 0
+arq = open('bar.txt', 'r')
+for linha in arq:
+    contador += linha.count('sir')
+arq.close()
+print(contador)
